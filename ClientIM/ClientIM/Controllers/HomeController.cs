08dfb1b155092745abca7d0e5db81e1a1502cd68
cstuser <lachezarpeychev.ca@gmail.com>
2020-02-18 11:06:58 -0500

    First Commit

---
+++ ClientIM/ClientIM/Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClientIM.Controllers
+{
+    public class HomeController : Controller
+    {
+        Models.ClientEntities6 db = new Models.ClientEntities6();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Clients);
+        }
+
+        public ActionResult setProfile(int id ) {
+            Models.Picture thePicture = db.Pictures.SingleOrDefault(c => c.picture_id == id);
+            Models.Client theClient = db.Clients.SingleOrDefault(c => c.person_id == thePicture.person_id);
+            theClient.profile_pic = thePicture.path;
+
+            db.SaveChanges();
+
+            return View("Index", db.Clients);
+        }
+
+        public ActionResult Search(String name)
+        {
+            IEnumerable < Models.Client > result = db.Clients.Where(p => (p.first_name + " " + p.last_name).Contains(name));
+            return View("Index", result);
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            Models.Client theClient = db.Clients.SingleOrDefault(c => c.person_id == id);
+            return View(theClient);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Models.Client newClient = new Models.Client();
+                newClient.first_name = collection["first_name"];
+                newClient.last_name = collection["last_name"];
+                newClient.notes= collection["notes"];
+                newClient.gender = collection["gender"];
+
+                db.Clients.Add(newClient);
+                db.SaveChanges();
+
+                
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var aClient = db.Clients.SingleOrDefault(c => c.person_id == id);
+            return View(aClient);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Models.Client theClient = db.Clients.SingleOrDefault(c => c.person_id == id);
+                theClient.first_name = collection["first_name"];
+                theClient.last_name = collection["last_name"];
+                theClient.notes = collection["notes"];
+                theClient.gender = collection["gender"];
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Models.Client theClient = db.Clients.SingleOrDefault(c => c.person_id == id);
+
+            return View(theClient);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Models.Client theClient = db.Clients.SingleOrDefault(c => c.person_id == id);
+                
+                db.Addresses.RemoveRange(theClient.Addresses);
+                db.Contacts.RemoveRange(theClient.Contacts);
+                db.Pictures.RemoveRange(theClient.Pictures);
+                db.Clients.Remove(theClient);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ClientIM/ClientIM/Models/ValaditingData.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClientIM.Models
+{
+    [MetadataType(typeof(ValaditingClient))]
+    public partial class Client
+    {
+
+    }
+
+    [MetadataType(typeof(ValaditingAddress))]
+    public partial class Address
+    {
+
+    }
+
+    [MetadataType(typeof(ValaditingContact))]
+    public partial class Contact
+    {
+
+    }
+
+    [MetadataType(typeof(ValaditingPicture))]
+    public partial class Picture
+    {
+
+    }
+
+    [MetadataType(typeof(ValaditingCountry))]
+    public partial class Country
+    {
+
+    }
+
+    public class ValaditingClient
+    {
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Please enter a name")]
+        [StringLength(50, MinimumLength = 1)]
+        public string first_name;
+        [Display(Name = "Last Name")]
+        public string last_name;
+        [Display(Name = "Note")]
+        public string notes;
+        [Display(Name = "Gender")]
+        public string gender;
+        [Display(Name = "Profile picture")]
+        public string profile_pic;
+    }
+
+    public class ValaditingAddress
+    {
+        [Display(Name = "Person Id")]
+        public string person_id;
+        [Display(Name = "Description")]
+        public string desc;
+        [Required(ErrorMessage = "Please enter a street")]
+        [Display(Name = "Street")]
+        public string street;
+        [Required(ErrorMessage = "Please enter a city")]
+        [Display(Name = "City")]
+        public string city;
+        [Display(Name = "State")]
+        public string state;
+        [Display(Name = "Zip Code")]
+        public string zip_code;
+    }
+    public class ValaditingContact
+    {
+        [Required]
+        [Display(Name = "Information Type")]
+        public string type_info;
+        [Required]
+        [Display(Name = "Information")]
+        public string info;
+    }
+    public class ValaditingPicture
+    {
+        [Display(Name = "Caption")]
+        public string caption;
+        [Required]
+        [Display(Name = "Path")]
+        public string path;
+        [Display(Name = "Time Information")]
+        public string time_info;
+        [Display(Name = "Location Information")]
+        public string loc_info;
+    }
+    public class ValaditingCountry
+    {
+        [Display(Name = "Country Name")]
+        public string country_name;
+        [Display(Name = "Country Id")]
+        public string country_id;
+    }
+}
