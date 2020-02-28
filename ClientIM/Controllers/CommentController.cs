57b3f59ecaea48dcd20fba408aaffd874f5bd581
Bryan Diego-Rivas <Bryan.dieogirvas@hotmail.com>
2020-02-27 20:47:32 -0500

    Added comment controller

--- ClientIM/Controllers/CommentController.cs
+++ ClientIM/Controllers/CommentController.cs
@@ -8,33 +8,45 @@ namespace ClientIM.Controllers
 {
     public class CommentController : Controller
     {
-        // GET: Comment
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        // GET: Comment/Details/5
-        public ActionResult Details(int id)
+        Models.ClientEntities db = new Models.ClientEntities();
+        // GET: Comment
+        public ActionResult Index(int id)
         {
-            return View();
+            ViewBag.id = id;
+
+            Models.Picture thePicutre = db.Pictures.SingleOrDefault(c => c.picture_id == id);
+
+            return View(thePicutre);
         }
 
         // GET: Comment/Create
-        public ActionResult Create()
+        public ActionResult Create(int id)
         {
+            ViewBag.id = db.Pictures.SingleOrDefault(p => p.picture_id == id);
+
             return View();
         }
 
         // POST: Comment/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(int id, FormCollection collection)
         {
             try
             {
                 // TODO: Add insert logic here
+                int personId = Int32.Parse(Session["person_id"].ToString());
+                Models.Comment newComment = new Models.Comment {
+                    picture_id = id,
+                    person_id = personId,
+                    comment1 = collection["comment1"],
+                    timestamp = DateTime.Now.ToString(),
+                    read = "Not read"
+                };
+                db.Comments.Add(newComment);
+                db.SaveChanges();
 
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Comment", new { id = id });
             }
             catch
             {
